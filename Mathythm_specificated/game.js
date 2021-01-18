c3ba5295c3e5d9eeeaa6275b280ba554ea8ae7b5
IDislikeName <54780075+IDislikeName@users.noreply.github.com>
2021-01-18 00:23:12 -0500

    gaming

--- Mathythm_specificated/game.js
+++ Mathythm_specificated/game.js
@@ -49,14 +49,36 @@ scene.create = function() {
 		steps+=1
 	})
 	this.levelText = this.add.text(350, 100+30*this.level, '- Level '+this.level+' -', { fontSize: '16px', fill: '#000' });
+	this.initialTime = 40;
+
+	this.text = this.add.text(32, 32, 'Countdown: ' + this.initialTime);
+	this.scoreText = this.add.text(550,32,"");
+
+    // Each 1000 ms call onEvent
+	timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+	this.score = 0;
 };
+function onEvent ()
+{
+	if(this.level>10){
+		this.initialTime -= 1; // One second
+    	this.text.setText('Countdown: ' + this.initialTime);
+	}
+	else{
+		this.text.setText("");
+	}
+    
+}
 
 scene.update = function() {
 	if(this.num==this.goal) {
 		this.level+=1
 		if (this.level>10){
+			this.score += Math.max(this.initialTime,0);
+			this.scoreText.setText("Score: " + this.score);
 			//endless mode starts
 			L  = this.level+1;
+			this.initialTime = 40;
 			k = Math.floor(Math.sqrt(this.level));
 			this.plus = Math.floor(Math.random()*L);
 			this.multiply = Math.floor(Math.random()*11);
@@ -72,23 +94,18 @@ scene.update = function() {
 					this.goal*=this.multiply
 				}
 			}
-			scene.plusText.setText('+'+this.plus);
-			scene.multiText.setText('x'+this.multiply);
-			scene.goalText.setText('Goal: '+this.goal);
-			scene.numText.setText('Current: '+this.num);
-			scene.levelText.setText('- Level '+this.level+' -');
 		}
 		else {
 		this.num = num_array[this.level-1]
 		this.goal = goal_array[this.level-1]
 		this.plus = plus_array[this.level-1]
 		this.multiply = multiply_array[this.level-1]
+		}
 		scene.plusText.setText('+'+this.plus);
 		scene.multiText.setText('x'+this.multiply);
 		scene.goalText.setText('Goal: '+this.goal);
 		scene.numText.setText('Current: '+this.num);
 		scene.levelText.setText('- Level '+this.level+' -');
-		}
 	}
 	else if (this.num>this.goal){
 		if(this.level<=10){
@@ -105,10 +122,14 @@ scene.update = function() {
 		scene.goalText.setText('Goal: '+this.goal);
 		scene.numText.setText('Current: '+this.num);
 	}
+	if(this.initialTime<=0){
+		this.end();
+	}
 };
 
 
 scene.end = function() {
 	this.add.text(250, 450, 'Congratulations!', { fontSize: '32px', fill: '#111' });
-	this.add.text(100, 550, 'Your spent ' + (steps-83) + ' more step(s) than the minimum.', { fontSize: '16px', fill: '#111' });
+	//this.add.text(100, 550, 'Your spent ' + (steps-83) + ' more step(s) than the minimum.', { fontSize: '16px', fill: '#111' });
+	this.add.text(100, 550, 'Your final score is ' + this.score, { fontSize: '16px', fill: '#111' });
 };
